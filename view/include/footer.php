af3a4243144820f021555d3c1df6e32fa45904cc
codewithdj <deepak_jain@persistent.com>
2017-08-18 11:35:59 +0530

    modegallery ready

---
+++ view/include/footer.php
@@ -0,0 +1,26 @@
+<hr>
+<footer>
+    <ul class="list-inline">
+     
+    </ul>
+</footer>
+
+<script>
+    $(function () {
+<?php
+if (!empty($_GET['error'])) {
+    ?>
+            swal({title: "Sorry!", text: "<?php echo $_GET['error']; ?>", type: "error", html: true});
+    <?php
+}
+?>
+    });
+</script>
+<script src="<?php echo $global['webSiteRootURL']; ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/seetalert/sweetalert.min.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/bootpag/jquery.bootpag.min.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/bootgrid/jquery.bootgrid.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>bootstrap/bootstrapSelectPicker/js/bootstrap-select.min.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/script.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/bootstrap-toggle/bootstrap-toggle.min.js" type="text/javascript"></script>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/js-cookie/js.cookie.js" type="text/javascript"></script>
